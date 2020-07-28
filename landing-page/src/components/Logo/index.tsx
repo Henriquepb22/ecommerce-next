7b2fc637639d5b80e9180cba2cedb6ccc2ce4dfa
Henriquepb22 <henriquepbdev@gmail.com>
2020-07-27 22:04:47 -0300

    Add landing page front-end, graphql request, types and queries

---
+++ landing-page/src/components/Logo/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import * as S from './styles'
+import { LogoProps } from 'types/api'
+
+const Logo = ({ url, alternativeText }: LogoProps) => (
+  <S.LogoWrapper src={`http://localhost:1337${url}`} alt={alternativeText} />
+)
+
+export default Logo
