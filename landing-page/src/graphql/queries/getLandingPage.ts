a43b44c388e6fb43237e9776b27447b16a10fec3
Henriquepb22 <henriquepbdev@gmail.com>
2020-07-28 22:34:17 -0300

    Add types definitions and queries for all contents

--- landing-page/src/graphql/queries/getLandingPage.ts
+++ landing-page/src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,146 @@
 const GET_LANDING_PAGE = /* GraphQL */ `
-  query GET_LANDING_PAGE {
-    landingPage {
-      logo {
+  fragment logo on LandingPage {
+    logo {
+      url
+      alternativeText
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        url
+        label
+      }
+      image {
         url
         alternativeText
       }
+    }
+  }
+
+  fragment sectionAboutProject on LandingPage {
+    sectionAboutProject {
+      image {
+        url
+        alternativeText
+      }
+      title
+      description
+    }
+  }
+
+  fragment sectionTech on LandingPage {
+    sectionTech {
+      title
+      techIcons {
+        icon {
+          alternativeText
+          url
+        }
+        title
+      }
+    }
+  }
+
+  fragment sectionConcepts on LandingPage {
+    sectionConcepts {
+      title
+      concepts {
+        title
+      }
+    }
+  }
+
+  fragment sectionModules on LandingPage {
+    sectionModules {
+      title
+      modules {
+        title
+        subtitle
+        description
+      }
+    }
+  }
+
+  fragment sectionAgenda on LandingPage {
+    sectionAgenda {
+      title
+      description
+    }
+  }
+
+  fragment pricingBox on LandingPage {
+    pricingBox {
+      totalPrice
+      numberInstallments
+      priceInstallment
+      benefits
+      button {
+        label
+        url
+      }
+    }
+  }
+
+  fragment sectionAboutUs on LandingPage {
+    sectionAboutUs {
+      title
+      authors {
+        photo {
+          url
+          alternativeText
+        }
+        name
+        role
+        socialLinks {
+          title
+          url
+        }
+        description
+      }
+    }
+  }
+
+  fragment sectionReviews on LandingPage {
+    sectionReviews {
+      title
+      reviews {
+        photo {
+          url
+          alternativeText
+        }
+        name
+        text
+      }
+    }
+  }
+
+  fragment sectionFaq on LandingPage {
+    sectionFaq {
+      title
+      questions {
+        question
+        answer
+      }
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
+      ...sectionAboutProject
+      ...sectionTech
+      ...sectionConcepts
+      ...sectionModules
+      ...sectionAgenda
+      ...pricingBox
+      ...sectionAboutUs
+      ...sectionReviews
+      ...sectionFaq
     }
   }
 `
--- landing-page/src/types/api.ts
+++ landing-page/src/types/api.ts
@@ -3,6 +3,132 @@ export type LogoProps = {
   alternativeText: string
 }
 
+export type HeaderProps = {
+  title: string
+  description: string
+  button: {
+    url: string
+    label: string
+  }
+  image: {
+    url: string
+    alternativeText: string
+  }
+}
+
+export type SectionAboutProjectProps = {
+  image: {
+    url: string
+    alternativeText: string
+  }
+  title: string
+  description: string
+}
+
+export type SectionTechProps = {
+  title: string
+  techIcons: [TechIcons]
+}
+
+export type TechIcons = {
+  title: string
+  icon: {
+    alternativeText: string
+    url: string
+  }
+}
+
+export type SectionConceptsProps = {
+  title: string
+  concepts: [Concepts]
+}
+
+export type Concepts = {
+  title: string
+}
+
+export type SectionModulesProps = {
+  title: string
+  modules: [Modules]
+}
+
+export type Modules = {
+  title: string
+  subtitle: string
+  description: string
+}
+
+export type SectionAgendaProps = {
+  title: string
+  description: string
+}
+
+export type PricingBoxProps = {
+  totalPrice: number
+  numberInstallments: number
+  priceInstallment: number
+  benefits: string
+  button: {
+    label: string
+    url: string
+  }
+}
+
+export type SectionAboutUsProps = {
+  title: string
+  authors: [Authors]
+}
+
+export type Authors = {
+  photo: {
+    url: string
+    alternativeText: string
+  }
+  name: string
+  role: string
+  socialLinks: [SocialLinks]
+  description: string
+}
+
+export type SocialLinks = {
+  title: string
+  url: string
+}
+
+export type SectionReviewsProps = {
+  title: string
+  reviews: [Reviews]
+}
+
+export type Reviews = {
+  photo: {
+    url: string
+    alternativeText: string
+  }
+  name: string
+  text: string
+}
+
+export type SectionFaqProps = {
+  title: string
+  questions: [Questions]
+}
+
+export type Questions = {
+  question: string
+  answer: string
+}
+
 export type LandingPageProps = {
   logo: LogoProps
+  header: HeaderProps
+  sectionAboutProject: SectionAboutProjectProps
+  sectionTech: SectionTechProps
+  sectionConcepts: SectionConceptsProps
+  sectionModules: SectionModulesProps
+  sectionAgenda: SectionAgendaProps
+  pricingBox: PricingBoxProps
+  sectionAboutUs: SectionAboutUsProps
+  sectionReviews: SectionReviewsProps
+  sectionFaq: SectionFaqProps
 }
