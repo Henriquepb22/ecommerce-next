d5a4481e857c1074d11d107b39875e35c9351ff7
Henriquepb22 <henriquepbdev@gmail.com>
2021-02-07 13:13:01 -0300

    Implements highlight component

---
+++ client/src/components/Highlight/stories.tsx
@@ -0,0 +1,30 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Highlight, { HightlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Read Dead it`s back',
+    subtitle: 'Come see John`s new adventures',
+    backgroundImage: '/img/red-dead-img.jpg',
+    buttonLabel: 'Buy now',
+    buttonLink: '/rdr2'
+  }
+} as Meta
+
+export const Default: Story<HightlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const WithFloatImage: Story<HightlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+WithFloatImage.args = {
+  floatImage: '/img/red-dead-float.png'
+}
