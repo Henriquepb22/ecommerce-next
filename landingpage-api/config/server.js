1d2a0420a59f160ad4896ea7276f25fd19a9bae2
Henriquepb22 <henriquepbdev@gmail.com>
2021-02-12 19:51:48 -0300

    Remove boilerplate and landing page files

--- landingpage-api/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '8ae72574bedd8232c37c63e4e6762979'),
-    },
-  },
-});
--- landingpage-api/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'e0c579f7-2246-4333-9649-dba8f1ab5ee4'
-};
