6316af740c4242ba662d8d473d90db07835719e3
Henriquepb22 <henriquepbdev@gmail.com>
2021-03-12 10:06:39 -0300

    Implements TextContent component

--- client/src/components/GameDetails/test.tsx
+++ client/src/components/GameDetails/test.tsx
@@ -13,7 +13,7 @@ const props: GameDetailsProps = {
 
 describe('<GameDetails />', () => {
   it('should render the blocks', () => {
-    renderWithTheme(<GameDetails {...props} />)
+    const { container } = renderWithTheme(<GameDetails {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /developer/i })
@@ -29,6 +29,8 @@ describe('<GameDetails />', () => {
     ).toBeInTheDocument()
     expect(screen.getByRole('heading', { name: /rating/i })).toBeInTheDocument()
     expect(screen.getByRole('heading', { name: /genres/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render platform icons', () => {
