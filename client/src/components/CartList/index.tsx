07c04e36707cef2c22e5620db3cfb63c55040ba3
Henriquepb22 <henriquepbdev@gmail.com>
2021-03-22 09:24:14 -0300

    Add new property to CartList component

--- client/src/components/CartList/index.tsx
+++ client/src/components/CartList/index.tsx
@@ -1,13 +1,16 @@
+import Button from 'components/Button'
 import GameItem, { GameItemProps } from 'components/GameItem'
+import Link from 'next/link'
 
 import * as S from './styles'
 
 export type CartListProps = {
   items: GameItemProps[]
   total: string
+  hasButton?: boolean
 }
 
-const CartList = ({ items, total }: CartListProps) => (
+const CartList = ({ items, total, hasButton = false }: CartListProps) => (
   <S.Wrapper>
     <>
       {items.map((item) => (
@@ -16,7 +19,14 @@ const CartList = ({ items, total }: CartListProps) => (
     </>
 
     <S.Footer>
-      Total <S.Total>{total}</S.Total>
+      {!hasButton && <span>Total:</span>}
+      <S.Total>{total}</S.Total>
+
+      {hasButton && (
+        <Link href="/cart">
+          <Button as="a">Buy it now</Button>
+        </Link>
+      )}
     </S.Footer>
   </S.Wrapper>
 )
