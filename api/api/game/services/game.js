0f204bc157d9747ef95116173978b58dc0a58acf
henriquepb22 <henriquepbdev@gmail.com>
2021-04-02 10:25:12 -0300

    Add Filter parser utils and fix games query

--- api/api/game/services/game.js
+++ api/api/game/services/game.js
@@ -4,6 +4,7 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
+
 const axios = require("axios");
 const slugify = require("slugify");
 const qs = require("querystring");
@@ -23,19 +24,10 @@ async function getGameInfo(slug) {
     const body = await axios.get(`https://www.gog.com/game/${slug}`);
     const dom = new JSDOM(body.data);
 
-    const ratingElement = dom.window.document.querySelector(
-      ".age-restrictions__icon use"
-    );
-
     const description = dom.window.document.querySelector(".description");
 
     return {
-      rating: ratingElement
-        ? ratingElement
-            .getAttribute("xlink:href")
-            .replace(/_/g, "")
-            .replace(/[^\w-]+/g, "")
-        : "FREE",
+      rating: "FREE",
       short_description: description.textContent.trim().slice(0, 160),
       description: description.innerHTML,
     };
@@ -61,32 +53,40 @@ async function create(name, entityName) {
 }
 
 async function createManyToManyData(products) {
-  const developers = {};
-  const publishers = {};
-  const categories = {};
-  const platforms = {};
+  const developers = new Set();
+  const publishers = new Set();
+  const categories = new Set();
+  const platforms = new Set();
 
   products.forEach((product) => {
     const { developer, publisher, genres, supportedOperatingSystems } = product;
 
-    genres && genres.forEach((genre) => (categories[genre] = true));
-    supportedOperatingSystems &&
-      supportedOperatingSystems.forEach((system) => (platforms[system] = true));
-    developers[developer] = true;
-    publishers[publisher] = true;
+    genres?.forEach((item) => {
+      categories.add(item);
+    });
+
+    supportedOperatingSystems?.forEach((item) => {
+      platforms.add(item);
+    });
+
+    developers.add(developer);
+    publishers.add(publisher);
   });
 
+  const createCall = (set, entityName) =>
+    Array.from(set).map((name) => create(name, entityName));
+
   return Promise.all([
-    ...Object.keys(developers).map((name) => create(name, "developer")),
-    ...Object.keys(publishers).map((name) => create(name, "publisher")),
-    ...Object.keys(categories).map((name) => create(name, "category")),
-    ...Object.keys(platforms).map((name) => create(name, "platform")),
+    ...createCall(developers, "developer"),
+    ...createCall(publishers, "publisher"),
+    ...createCall(categories, "category"),
+    ...createCall(platforms, "platform"),
   ]);
 }
 
 async function setImage({ image, game, field = "cover" }) {
   try {
-    const url = `https:${image}_bg_crop_1680x655.jpg`;
+    const url = `https:${image}.jpg`;
     const { data } = await axios.get(url, { responseType: "arraybuffer" });
     const buffer = Buffer.from(data, "base64");
 
@@ -162,6 +162,7 @@ module.exports = {
       const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&${qs.stringify(
         params
       )}`;
+
       const {
         data: { products },
       } = await axios.get(gogApiUrl);
