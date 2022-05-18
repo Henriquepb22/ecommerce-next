12195d0ec2bbe391fb3572053ebb2fa73f2c10d4
Henrique Pereira <henriquepbdev@gmail.com>
2022-05-18 15:08:10 -0300

    feat: add profile data from api

--- client/src/pages/profile/me.tsx
+++ client/src/pages/profile/me.tsx
@@ -1,21 +1,29 @@
 import { GetServerSidePropsContext } from 'next'
 
+import { QueryProfileMe } from 'graphql/generated/QueryProfileMe'
+import { QUERY_PROFILE_ME } from 'graphql/queries/profile'
 import protectedRoutes from 'utils/protected-routes'
-import FormProfile from 'components/FormProfile'
+import FormProfile, { FormProfileProps } from 'components/FormProfile'
+import { initializeApollo } from 'utils/apollo'
 import Profile from 'templates/Profile'
 
-export default function Me() {
+export default function Me(props: FormProfileProps) {
   return (
     <Profile>
-      <FormProfile />
+      <FormProfile {...props} />
     </Profile>
   )
 }
 
 export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   const session = await protectedRoutes(ctx)
+  const apolloClient = initializeApollo(null, session)
+
+  const { data } = await apolloClient.query<QueryProfileMe>({
+    query: QUERY_PROFILE_ME
+  })
 
   return {
-    props: { session }
+    props: { session, username: data.me?.username, email: data.me?.email }
   }
 }
